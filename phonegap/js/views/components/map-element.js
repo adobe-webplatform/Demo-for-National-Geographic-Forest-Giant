2a0e6d8f0a85eb59161edfc12ee8f2db8ef8c6ff
adeveria <adeveria@gmail.com>
2013-04-30 13:40:29 -0700

    google map control modifications

--- phonegap/js/views/components/map-element.js
+++ phonegap/js/views/components/map-element.js
@@ -13,14 +13,6 @@ define([], function (require) {
             filter = null,
             allowDrawing = false;
 
-        /*
-        function addOverlays() {
-            console.log('add overlays');
-            $body.append($container).append($mapPage);
-            
-        }
-        */
-        
         function resetFilter() {
             filter = {
                 curlPosition: 1
@@ -33,11 +25,8 @@ define([], function (require) {
             $mapPage.css('webkitFilter',
             'custom(url(assets/shaders/page-curl.vs) mix(url(assets/shaders/page-curl.fs) normal source-atop), 50 50 border-box, transform perspective(1000) scale(1) rotateX(0deg) rotateY(0deg) rotateZ(0deg), curlPosition ' + filter.curlPosition + ' 0, curlDirection 135, curlRadius 0.2, bleedThrough 0.5)');
         }
-        
 
         function openResolve() {
-            console.log('open resolve');
-            
             instance.showMap();
             $mapPage.hide();
             allowDrawing = false;
@@ -83,7 +72,7 @@ define([], function (require) {
             } else {
                 // Show gmap
                 TweenMax.to(filter, 0.5, {
-                    curlPosition: -1,
+                    curlPosition: -1.5,
                     onComplete: openResolve
                 });
             }
@@ -94,13 +83,13 @@ define([], function (require) {
         
         function addBackButton() {
 
-            var button = $('<button id="map-back-button">Back</button>')[0];
+            var button = $('<button id="map-back-button"></button>')[0];
             
-            googleMap.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(button);
+            googleMap.controls[google.maps.ControlPosition.TOP_RIGHT].push(button);
             google.maps.event.addDomListener(button, 'click', function() {
                 $mapPage.show();
                 allowDrawing = true;
-                filter.curlPosition = -1;
+                filter.curlPosition = -1.5;
                 instance.draw();
                 
                 TweenMax.to(filter, 1.5, {
@@ -115,7 +104,11 @@ define([], function (require) {
             var mapOptions = {
                 center: new google.maps.LatLng(36.4333, -118.6833),
                 zoom: 8,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                mapTypeControlOptions: {
+                    style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+                    position: google.maps.ControlPosition.TOP_CENTER
+                }
             };
             
             googleMap = new google.maps.Map($container[0], mapOptions);
