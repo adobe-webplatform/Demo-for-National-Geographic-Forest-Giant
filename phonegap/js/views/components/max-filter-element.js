65c163e20dfa9ffb12cf9509701916b6a6e69170
adeveria <adeveria@gmail.com>
2013-05-04 19:08:05 -0700

    filter progress

--- phonegap/js/views/components/max-filter-element.js
+++ phonegap/js/views/components/max-filter-element.js
@@ -46,6 +46,8 @@ define([], function (require) {
             deltaRotation = 0,
             foldLevel = 0, // 0 = folded in half, 1 = unfolded
             SCALE_DIVIDER = 100,
+            FOLD_PREPARE_TIME = 0.3, // time from init to dragging
+            fakeDistance = 700,
             basefilter = {
                 ambient: 1,
                 vert: filterPath + 'max-fold.vs',
@@ -130,7 +132,7 @@ define([], function (require) {
             $filterEl.css({
                 'webkitFilter': str
             });
-            console.log(str);
+            console.log('rotateY', filter.rotateY);
         }
         
         window.updateFilter = updateFilter; // debug
@@ -176,6 +178,7 @@ define([], function (require) {
             dragging = false;
             console.log('filter', filter);
             updateFilter(); // Needed for last frame
+
             
             return; //debug
 
@@ -190,8 +193,11 @@ define([], function (require) {
         function closeResolve() {
 
             console.log('close resolve');
-
+            
             dragging = false;
+            rotateTween.seek(0);
+            linearFoldTimeline.seek(0);
+            
             $container.css({'pointer-events': 'none'});
             $filterEl.css({opacity: 0});
             $el.css({opacity: 1});
@@ -211,7 +217,7 @@ define([], function (require) {
             }) );
             linearFoldTimeline.pause();
             
-            rotateTween = new TweenMax.to(filter, 1, {
+            rotateTween = new TweenMax.to(filter, FOLD_PREPARE_TIME, {
                 rotateY: 0,
                 ease: Linear.easeNone,
                 onComplete: rotateResolve
@@ -229,21 +235,67 @@ define([], function (require) {
 
             $body.unbind('touchend');
             $body.unbind('touchmove');
+            
+            console.log('foldLevel',foldLevel);
+            if( foldLevel < 0.4 ) {
+                // animate to folded
+                closeTween = new TweenMax.to(filter, 0.5, {
+                    x: basefilter.x, 
+                    y: basefilter.y, 
+                    scale: basefilter.scale,
+                    a9: basefilter.a9,
+                    a10: basefilter.a10,
+                    rotateY: basefilter.rotateY,
+                    onComplete: closeResolve
+                });
 
-
+            } else {
+                // animate to unfolded
+                openTween = new TweenMax.to(filter, 0.5, {
+                    x: 0, 
+                    y: 0, 
+                    scale: 1,
+                    a9: 0,
+                    a10: 0,
+                    rotateY: 0,
+                    onComplete: openResolve
+                });
+            }
         }
 
         function handle_filter_TOUCHMOVE(e) {
             var touches = e.originalEvent.touches,
                 t1, t2;
+                
+            console.log('handle_filter_TOUCHMOVE');
 
             e.preventDefault();
             e.stopPropagation();
+
+            // For desktop debug only!
+            var fakeTouches = [
+                touches[0],
+                {
+                    pageX: touches[0].pageX + fakeDistance,
+                    pageY: touches[0].pageY
+                }
+            ];
+            touches = fakeTouches;
 
             if (touches.length == 2) {
 
                 t1 = {x: touches[0].pageX, y: touches[0].pageY};
                 t2 = {x: touches[1].pageX, y: touches[1].pageY};
+                
+                var distance = Math.abs(touches[0].pageX - touches[1].pageX);
+                foldLevel = distance / window.innerWidth;
+
+                var newPosition = getTranslate(t1, t2);
+                var unfoldValues = getUnfoldValues(foldLevel);
+                
+                filter.x = newPosition.x;
+                filter.a9 = unfoldValues.a9;
+                filter.a10 = unfoldValues.a10;
             }
         }
         
@@ -253,6 +305,15 @@ define([], function (require) {
                 x: newMidpoint.x / window.innerWidth - 0.5,
                 y: newMidpoint.y / window.innerHeight - 0.5
             };
+        }
+        
+        function getUnfoldValues(newLevel) {
+            var n = 1 - newLevel;
+            var values = {
+                a9: n * -89,
+                a10: n * 90
+            };
+            return values;
         }
         
         function calculateMidpoint(t1, t2) {
@@ -265,27 +326,77 @@ define([], function (require) {
             var touches = e.originalEvent.touches,
                 t1, t2,
                 distance;
+                
+            // For desktop debug only!
+            var fakeTouches = [
+                touches[0],
+                {
+                    pageX: touches[0].pageX + fakeDistance,
+                    pageY: touches[0].pageY
+                }
+            ];
+            touches = fakeTouches;
+
 
             if (touches.length == 2) {
 
                 console.log('el touchstart');
+                
+                $body.bind('touchend', handle_filter_TOUCHEND);
+                $body.bind('touchmove', handle_filter_TOUCHMOVE);
 
                 e.preventDefault();
                 e.stopPropagation();
                 scroll.disable();
-                
+                resetFilter();
+                showFilterElement();
+                instance.startRequestAnimationFrame();
+
                 t1 = {x: touches[0].pageX, y: touches[0].pageY};
                 t2 = {x: touches[1].pageX, y: touches[1].pageY};
-                distance = getDistance(t1, t2);
+                
+                var distance = Math.abs(touches[0].pageX - touches[1].pageX);
+                foldLevel = distance / window.innerWidth;
+
+                var newPosition = getTranslate(t1, t2);
+                var unfoldValues = getUnfoldValues(foldLevel);
+                
+                // Tween to folded drag position
+                var moveTween = new TweenMax.to(filter, FOLD_PREPARE_TIME / 2, {
+                    x: newPosition.x,
+//                    y: newPosition.y,
+                    ease: Linear.easeNone,
+                    a9: unfoldValues.a9,
+                    a10: unfoldValues.a10,
+                    onComplete: function() {
+                        setTimeout(function() {
+                            console.log('filter', filter);
+                        }, 1000);
+                    }
+                })
+                dragging = true;
+
+                moveTween.play();
+                rotateTween.seek(0).play();
             }
         }
-
+/*
         function handle_resolveEl_TOUCHSTART(e) {
             var touches = e.originalEvent.touches,
                 t1, t2;
     
             //e.preventDefault();
             e.stopPropagation();
+            
+            // For desktop debug only!
+            var fakeTouches = [
+                touches[0],
+                {
+                    pageX: touches[0].pageX + fakeDistance,
+                    pageY: touches[0].pageY
+                }
+            ];
+            touches = fakeTouches;
 
             if (touches.length == 2) {
 
@@ -293,11 +404,24 @@ define([], function (require) {
              
                 t1 = {x: touches[0].pageX, y: touches[0].pageY};
                 t2 = {x: touches[1].pageX, y: touches[1].pageY};
+                var newPosition = getTranslate(t1, t2);
+                
+                // Tween to folded drag position
+                var moveTween = new TweenMax.to(filter, FOLD_PREPARE_TIME, {
+                    x: newPosition.x,
+                    y:newPosition.y,
+                    ease: Linear.easeNone
+                })
+                moveTween.play();
+                rotateTween.play();
+                var timelinePosition = 0.5;
+                linearFoldTimeline.timeScale(1 / FOLD_PREPARE_TIME);
+                linearFoldTimeline.tweenTo(timelinePosition);
 
                 dragging = true;
             }
         }
-
+*/
         function handle_resolveEl_CLICK(e) {
             console.log('resolve click');
 
@@ -324,7 +448,7 @@ define([], function (require) {
             $body = $('body');
 
             $el.bind('touchstart', handle_el_TOUCHSTART);
-            $el.bind('click', handle_el_CLICK);
+            // $el.bind('click', handle_el_CLICK);
             
             for (i = 0; i < 20; i += 1) {
                 if( i == 9 || i == 10 ) {
